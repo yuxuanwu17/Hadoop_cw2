de4013eda3468099cdecd54381725c6aceef0884
Yuxuan <yuxuan.wu17@gmail.com>
2020-11-06 20:13:20 +0800

    delete some files and ready to set the local file

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -14,127 +16,76 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordCount {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+            extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        /**
-         *
-         * @param key k1 行偏移量
-         * @param value v1 每一行的文本数据
-         * @param context 表示上下文的对象，桥梁，将上下给联系起来
-         * @throws IOException
-         * @throws InterruptedException
-         * 这里是为了改写map的function，将k1，v1 转换为k2，v2
-         */
+        // This is the method
+        public String deleteNotation(String words) {
+            String regEx = "[\\W[0-9]]";
 
-        /*
-        * 如何将k1 v1 转为k2， v2
-        * k1      v1
-        * 0       hello, world, hadoop 之类的文字数据
-        * --------------------------------
-        * k2            v2
-        * hello           1
-        * world           1
-        * hadop           1
-        * */
-
+            Pattern p = Pattern.compile(regEx);
+            Matcher matcher = p.matcher(words);
+            return matcher.replaceAll("").trim();
+        }
+//        This is the mapping method
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            //1: 将一行的文本数据进行拆分
-            StringTokenizer tokens = new StringTokenizer(value.toString());
-//            String txt_content = value.toString();
-//            String [] each_word = txt_content.split("\\s+");
 
+            ArrayList<String> bigrams = new ArrayList<String>();
+            String[] single_word = value.toString().split("\\s+");
 
-            //2：遍历数组，组装k2和v2
-            //3：将k2 和v2 写入下文（context）
+            for (int i = 0; i < single_word.length - 1; i++) {
+                // note that, in this case we should constrain the text length into length -1
+                // since this is the bigram case, there is length - 1 bigram words in total
+                // otherwise, it could case break the string
+                single_word[i] = deleteNotation(single_word[i]);
+                single_word[i + 1] = deleteNotation(single_word[i + 1]);
+                if (!(single_word[i].isEmpty()) && !(single_word[i + 1].isEmpty())) {
+                    bigrams.add(single_word[i] + " " + single_word[i + 1]);
+                }
 
-            while (tokens.hasMoreTokens()) {
-                String s = tokens.nextToken();
-                String del = "DELIMIT";
-                //process the token
-                s = s.toLowerCase();
-
-                //maintain placeholders for sentence-delimiters
-                s = s.replaceAll("\\?", del);
-                s = s.replaceAll("!", del);
-                s = s.replaceAll("\\.", del);
-
-                //nuke all non-word characters
-                s = s.replaceAll("[^\\w]", " ");
-                s = s.replaceAll("_", " ");
-
-                //use a period for the delimit (not really necessary)
-                s = s.replaceAll(del, " . ");
-
-                //nuke any extra whitespace (not really necessary)
-                s = s.replaceAll("\\s+", " ");
-
-                //split by spaces
-                String arr[] = s.split(" ");
-                word.set(tokens.nextToken());
-                context.write(word, one);
+            }
+            for (String token : bigrams) {
+                System.out.print(token);
+                word.set(token);
+                context.write(word,one);
+                // seventh STEP: context is aimed to collect Mapper output pairs. So, context.write(key, value) Add a (key, value) pair to the context context.write(word, ONE);
             }
         }
-    }
 
-    public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
-        /**
-         * reduce 方法的作用： 将新的k2 和 V2 转化为k3和v3 ，将k3和v3写入context中
-         * 参数：
-         *  key: 新的k2
-         *  values： 新的集合V2
-         *  context：表示上下文的对象
-         *  --------------
-         *  如何将新的k2和v2转化为k3和v3
-         *  hello <1,1,1>
-         *  world <1,1>
-         *  hadoop <1>
-         *  --------------
-         *  k3 and v3
-         *  hell0   3
-         *  world   2
-         *  hadoop  1
-         *  --------------
-         * 将集合里面的数字全部相加就成为了我们的v3         *
-         */
-        private IntWritable result = new IntWritable();
+        public static class IntSumReducer
+                extends Reducer<Text, IntWritable, Text, IntWritable> {
+            private IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
-            // 1： 遍历集合，将集合中的数字相加，得到我们的v3
+            public void reduce(Text key, Iterable<IntWritable> values,
+                               Context context
+            ) throws IOException, InterruptedException {
 
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+                int sum = 0;
+                for (IntWritable val : values) {
+                    sum += val.get();
+                }
+                result.set(sum);
+                context.write(key, result);
             }
-
-            //2: 将k3和v3写入上下文中
-            result.set(sum);
-            context.write(key, result);
         }
-    }
 
-    public static void main(String[] args) throws Exception {
-        /**
-         * 定义主类，并且提交脚本任务到hadoop中进行运行
-         */
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count"); //创建一个job的任务对象
+        public static void main(String[] args) throws Exception {
+            Configuration conf = new Configuration();
+            Job job = Job.getInstance(conf, "Bigram count");
+            job.setJarByClass(WordCount.class); //My class name is 'word count'
+            job.setMapperClass(TokenizerMapper.class); // Set my Mapper class
+            job.setCombinerClass(IntSumReducer.class); // Set my Combiner class
+            job.setReducerClass(IntSumReducer.class); // Set my Reducer class
+            job.setOutputKeyClass(Text.class); // Set the key class for my output class
+            job.setOutputValueClass(IntWritable.class); // Set the value class for the output data
 
-        //2： 配置job任务的8个步骤
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }
     }
 }
