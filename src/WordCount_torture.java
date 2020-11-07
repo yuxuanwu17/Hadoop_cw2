9b3e36793e2a828de3f57df6575ebb3b0e703706
Yuxuan <yuxuan.wu17@gmail.com>
2020-11-07 14:39:08 +0800

    WordCount_torture.java complete

--- src/WordCount_torture.java
+++ src/WordCount_torture.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -12,7 +11,9 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount_torture {
 
@@ -20,11 +21,15 @@ public class WordCount_torture {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "torture count");
         job.setJarByClass(WordCount_torture.class); //My class name is 'word count'
-        job.setMapperClass(TokenizerMapper.class); // Set my Mapper class
 
-        job.setReducerClass(TokenizerMapper.IntSumReducer.class); // Set my Reducer class
-        job.setOutputKeyClass(Text.class); // Set the key class for my output class
-        job.setOutputValueClass(IntWritable.class); // Set the value class for the output data
+        job.setMapperClass(TokenizerMapper.class); // Set my Mapper class
+        job.setReducerClass(IntSumReducer.class); // Set my Reducer class
+
+        job.setOutputKeyClass(Text.class);  // Set the key class for my output class
+        job.setOutputValueClass(Text.class); // Set the value class for the output data
+
+        job.setInputFormatClass(TextInputFormat.class);// Set input format for this job
+        job.setOutputFormatClass(TextOutputFormat.class); // Set output format for this job
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -34,52 +39,38 @@ public class WordCount_torture {
     public static class TokenizerMapper
             extends Mapper<LongWritable, Text, Text, Text> {
 
-
         private Text word = new Text();
 
         public void map(LongWritable key, Text value, Context context
         ) throws IOException, InterruptedException {
 
-//            String line = value.toString();
             String line[] = value.toString().split("\\n{1,}");
-//            String tmp[] = line.split("\\n{1,}");
-
-            ArrayList<String> results = new ArrayList<String>();
+            ArrayList<String> results = new ArrayList<>();
 
             for (int i = 0; i < line.length; i++) {
                 if (line[i].contains("torture")) {
                     results.add(line[i]);
                 }
             }
-//            for (int i = 0; i < tmp.length; i++) {
-//                if(tmp[i].contains("torture")){
-//                    results.add(line);
-//                }
-//            }
+
 
             for (String result : results) {
                 word.set(result);
-                context.write(word,new Text(" "));
+                context.write(word, new Text(" "));
             }
         }
-
-        public static class IntSumReducer
-                extends Reducer<Text, IntWritable, Text, IntWritable> {
-            private IntWritable result = new IntWritable();
-
-            public void reduce(Text key, Iterable<IntWritable> values,
-                               Context context
-            ) throws IOException, InterruptedException {
-
-                int sum = 0;
-                for (IntWritable val : values) {
-                    sum += val.get();
-                }
-                result.set(sum);
-                context.write(key, result);
-            }
-        }
-
-
     }
+
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, Text> {
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+
+            context.write(new Text(key), new Text(" "));
+
+        }
+    }
+
+
 }
