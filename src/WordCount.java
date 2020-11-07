8ff0cd6b7f9c867aa8e14cd4625f36830b8eeca0
Yuxuan <yuxuan.wu17@gmail.com>
2020-11-07 09:38:59 +0800

    Success!

--- src/WordCount.java
+++ src/WordCount.java
@@ -20,7 +20,7 @@ public class WordCount {
         Job job = Job.getInstance(conf, "Bigram count");
         job.setJarByClass(WordCount.class); //My class name is 'word count'
         job.setMapperClass(TokenizerMapper.class); // Set my Mapper class
-//        job.setCombinerClass(TokenizerMapper.IntSumReducer.class); // Set my Combiner class
+
         job.setReducerClass(TokenizerMapper.IntSumReducer.class); // Set my Reducer class
         job.setOutputKeyClass(Text.class); // Set the key class for my output class
         job.setOutputValueClass(IntWritable.class); // Set the value class for the output data
@@ -36,14 +36,35 @@ public class WordCount {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
+        // The method is to extract useful word
+        public String deleteNotation(String words) {
+            String regEx = "[\\W[0-9]]";
+            Pattern p = Pattern.compile(regEx);
+            Matcher matcher = p.matcher(words);
+            return matcher.replaceAll("").trim();
+        }
+
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
             ArrayList<String> bigrams = new ArrayList<String>();
             String[] single_word = value.toString().split("\\s+");
-            for (String token : single_word) {
+
+            // create and fill the bigram arraylist
+            // the bigram has number of (single_word -1) since it has two words
+            for (int i = 0; i < single_word.length - 1; i++) {
+                single_word[i] = deleteNotation(single_word[i]);
+                single_word[i + 1] = deleteNotation(single_word[i + 1]);
+                if (!(single_word[i].isEmpty()) && !(single_word[i + 1].isEmpty())) {
+                    bigrams.add(single_word[i] + " " + single_word[i + 1]);
+                }
+
+            }
+            for (String token : bigrams) {
+//                System.out.print(token);
                 word.set(token);
                 context.write(word, one);
             }
+
         }
 
         public static class IntSumReducer
